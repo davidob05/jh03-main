37973f8de2e756ad9368fbde85ff159ee02f428d
David O'Brien <davidobrien093@gmail.com>
2025-11-05 20:49:41 +0000

    add django smoke test

--- services/django/lithium/pages/tests.py
+++ services/django/lithium/pages/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_home_page_renders(self):
+        response = self.client.get(reverse("home"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/home.html")
+
+
+class AboutPageTests(TestCase):
+    def test_about_page_renders(self):
+        response = self.client.get(reverse("about"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/about.html")
