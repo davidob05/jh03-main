05ee838a00d00365ecfd7c296472d90def44f7e9
David O'Brien <davidobrien093@gmail.com>
2025-11-13 23:55:25 +0000

    cleaned directories and added file parsing view

--- services/django/lithium/django_project/urls.py
+++ services/django/lithium/django_project/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", include("exams.urls")),
+    path("", include("timetabling_system.urls")),
 ]
 
 if settings.DEBUG:
