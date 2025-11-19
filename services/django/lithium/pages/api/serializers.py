ed6cc4e2733ba5d1cf3a5c930d1ca12a3e890acc
David O'Brien <davidobrien093@gmail.com>
2025-11-19 11:24:27 +0000

    merged file handling and REST setup

--- services/django/lithium/pages/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from pages.models import Exam
-
-class ExamSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Exam
-        fields = "__all__"
--- services/django/lithium/pages/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from pages.models import Exam
-from .serializers import ExamSerializer
-
-class ExamViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Exam.objects.all()
-    serializer_class = ExamSerializer
