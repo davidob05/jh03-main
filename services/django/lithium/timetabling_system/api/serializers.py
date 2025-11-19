0edd02baa6551763472f11c10f22c8cba364fd27
David O'Brien <davidobrien093@gmail.com>
2025-11-19 11:30:41 +0000

    build should pass

--- services/django/lithium/timetabling_system/api/serializers.py
+++ services/django/lithium/timetabling_system/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from pages.models import Exam
+from timetabling_system.models import Exam
 
 class ExamSerializer(serializers.ModelSerializer):
     class Meta:
