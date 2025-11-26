8252b2e371ded654c86d60130a48027daa65a56b
leonard-h <leonardhockerts@gmail.com>
2025-11-26 10:42:36 +0000

    exams table

--- services/django/lithium/timetabling_system/api/serializers.py
+++ services/django/lithium/timetabling_system/api/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from timetabling_system.models import Exam
 
+
 class ExamSerializer(serializers.ModelSerializer):
+    venues = serializers.SerializerMethodField()
+
     class Meta:
         model = Exam
         fields = "__all__"
+
+    def get_venues(self, obj):
+        """Return venue names associated with an exam via ExamVenue."""
+        exam_venues = getattr(obj, "_prefetched_objects_cache", {}).get("examvenue_set")
+        if exam_venues is None: exam_venues = obj.examvenue_set.select_related("venue").all()
+        return [ev.venue.venue_name for ev in exam_venues]
