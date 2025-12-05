ea0adb10a2858d48d64bcc5449ce294489fb2753
David O'Brien <2854863o@student.gla.ac.uk>
2025-12-05 10:20:02 +0000

    Page design merged to main

--- services/frontend/app/src/Routes.tsx
+++ services/frontend/app/src/Routes.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material";
 import React, { useMemo } from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route as RouterRoute } from "react-router-dom";
@@ -45,32 +44,9 @@ export const Routes: React.FC = () => {
             <RouterRoute path="invigilators" element={<AdminInvigilators />} />
             <RouterRoute path="invigilators/:id" element={<AdminInvigilatorProfile />} />
           </RouterRoute>
-          <RouterRoute path="/admin/exams" element={<AdminLayout />}>
-          <RouterRoute path="/admin/exams" element={<AdminLayout />}>
-            <RouterRoute index element={<Exams />} />
-          </RouterRoute>
-          <RouterRoute path="/admin/venues" element={<AdminLayout />}>
-          <RouterRoute path="/admin/venues" element={<AdminLayout />}>
-            <RouterRoute index element={<Venues />} />
-          </RouterRoute>
-          <RouterRoute path="/admin/calendar" element={<AdminLayout />}>
-          <RouterRoute path="/admin/calendar" element={<AdminLayout />}>
-            <RouterRoute index element={<Calendar />} />
-          </RouterRoute>
-          <RouterRoute path="/admin/profile" element={<AdminLayout />}>
-          <RouterRoute path="/admin/profile" element={<AdminLayout />}>
-            <RouterRoute index element={<Profile />} />
-          </RouterRoute>
-          <RouterRoute path="/admin/invigilators" element={<AdminLayout />}>
-          <RouterRoute path="/admin/invigilators" element={<AdminLayout />}>
-            <RouterRoute index element={<AdminInvigilators />} />
-          </RouterRoute>
-          <RouterRoute path="/exams/:examId" element={<AdminLayout />}>
-            <RouterRoute index element={<AdminExams />} />
-          </RouterRoute>
 
           {/* Invigilator Pages */}
-          <RouterRoute path="/invigilator" element={<InvigilatorAdminLayout />}>
+          <RouterRoute path="/invigilator" element={<InvigilatorLayout />}>
             <RouterRoute index element={<InvigilatorDashboard />} /> 
             {/* <RouterRoute path="timetable" element={<InvigilatorTimetable />} />
             <RouterRoute path="profile" element={<InvigilatorProfile />} /> */}
