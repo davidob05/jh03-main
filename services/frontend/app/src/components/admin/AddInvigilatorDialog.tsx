ffcb928472da0441ebc3bae56c60b290c40c0038
Aaron Hughes <2881785h@student.gla.ac.uk>
2025-12-19 12:46:01 +0000

    Minor tweaks to dialog

--- services/frontend/app/src/components/admin/AddInvigilatorDialog.tsx
+++ services/frontend/app/src/components/admin/AddInvigilatorDialog.tsx
@@ -44,9 +44,9 @@ const RESTRICTION_CHOICES = [
   { value: "computer_cluster", label: "Computer cluster", yes: "Can work in computer clusters", no: "Cannot work in computer clusters" },
   { value: "vet_school", label: "Vet School", yes: "Can work at the Vet School", no: "Cannot work at the Vet School" },
   { value: "sec", label: "Scottish Event Campus", yes: "Can work at the SEC", no: "Cannot work at the SEC" },
-  { value: "osce_golden_jubilee", label: "OSCE - Golden Jubilee", yes: "Can work at Golden Jubilee", no: "Cannot work at Golden Jubilee" },
-  { value: "osce_wolfson", label: "OSCE - Wolfson", yes: "Can work at Wolfson", no: "Cannot work at Wolfson" },
-  { value: "osce_queen_elizabeth", label: "OSCE - Queen Elizabeth", yes: "Can work at Queen Elizabeth", no: "Cannot work at Queen Elizabeth" },
+  { value: "osce_golden_jubilee", label: "Golden Jubilee", yes: "Can work at Golden Jubilee", no: "Cannot work at Golden Jubilee" },
+  { value: "osce_wolfson", label: "Wolfson", yes: "Can work at Wolfson", no: "Cannot work at Wolfson" },
+  { value: "osce_queen_elizabeth", label: "Queen Elizabeth", yes: "Can work at Queen Elizabeth", no: "Cannot work at Queen Elizabeth" },
   { value: "approved_exemption", label: "Approved exemption", yes: "Has approved exemption", no: "No exemption" },
 ];
 
