23f1d7869705e172152a9d98f38edb5093ef7143
Aaron Hughes <2881785h@student.gla.ac.uk>
2026-01-16 18:38:03 +0000

    Removal of Afternoon slot for invigilator availability

--- services/django/lithium/timetabling_system/models.py
+++ services/django/lithium/timetabling_system/models.py
@@ -68,7 +68,6 @@ class DietChoices(models.TextChoices):
 
 class SlotChoices(models.TextChoices):
     MORNING = 'MORNING', 'Morning (AM)'
-    AFTERNOON = 'AFTERNOON', 'Afternoon (Noon)'
     EVENING = 'EVENING', 'Evening (PM)'
 
 
